564cad636627797d05f590b16ee34c651d645e1b
Timo Furrer <tuxtimo@gmail.com>
2017-12-24 12:21:12 +0100

    Add optimized solution for part 2 of day 23

--- year_2017/day_23/solution_part_2.py
+++ year_2017/day_23/solution_part_2.py
@@ -2,52 +2,20 @@
 Solution for the first puzzle of Day 23
 """
 
-import os
-from collections import defaultdict
-
 import pytest
 
-# My puzzle input
-MY_PUZZLE_INPUT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
-
-def is_number(x):
-    try:
-        int(x)
-    except ValueError:
-        return False
-    else:
-        return True
-
-
-def parse_instructions(raw_instructions):
-    instructions = []
-    for raw_instruction in raw_instructions.splitlines():
-        instructions.append(raw_instruction.split())
-    return instructions
-
-
-def evaluate_register_h(instructions):
-    b = c = d = e = f = h = 0
-
+def count_primes():
+    # input values
     b = 108100
     c = 125100
 
+    h = 0
     while True:
-        # f = 1
         f = True
-        d = 2
-        while True:
-            e = 2
-            while True:
-                if d * e == b:
-                    f = False
-                e = e + 1
-                if e == b:
-                    break
-
-            d = d + 1
-            if d == b:
+        for d in range(2, b):
+            if b % d == 0:
+                f = False
                 break
 
         if f is False:
@@ -60,18 +28,14 @@ def evaluate_register_h(instructions):
     return h
 
 
-@pytest.mark.parametrize('input_file, expected_register_h_value', [
-    # my puzzle input
-    (MY_PUZZLE_INPUT, 0)
+@pytest.mark.parametrize('expected_register_h_value', [
+    909
 ])
-def test_evaluate_register_h(input_file, expected_register_h_value):
+def test_count_primes(expected_register_h_value):
     """Test the solution"""
     # given
-    with open(input_file) as f:
-        instructions = parse_instructions(f.read())
-
     # when
-    actual_register_h_value = evaluate_register_h(instructions)
+    actual_register_h_value = count_primes()
 
     # then
     assert actual_register_h_value == expected_register_h_value
